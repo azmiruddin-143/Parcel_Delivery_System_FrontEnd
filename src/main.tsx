52c033b1956413fde92e101d150d3b1105189846
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2025-08-22 00:43:06 +0600

    Project Setup and header-footer Complete

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router'
+import { router } from './routes'
+import { ThemeProvider } from './providers/theme.provider'
+import { Toaster } from "sonner";
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
+  </React.StrictMode>,
+)
