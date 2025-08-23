2f11ea0d2619913744a2621bd6fb060621fe6d5c
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2025-08-23 18:20:39 +0600

    all role er page create

---
+++ src/components/layout/DashboardLayput.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+
+
+const DashboardLayput = () => {
+    return (
+        <div>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default DashboardLayput;
